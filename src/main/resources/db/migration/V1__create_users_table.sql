35233fb8a13182af85046d31ce09695c602af2ab
Junior Zytkowski <junior.zytkowski@gmail.com>
2020-06-24 22:57:54 -0300

    Added password field

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE public."user"
     user_id    uuid                     NOT NULL,
     username   character varying(100)   NOT NULL,
     email      character varying(255)   NOT NULL,
+    password   character varying(500)   NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT now(),
     updated_at timestamp with time zone,
     CONSTRAINT pk_user PRIMARY KEY (user_id),
