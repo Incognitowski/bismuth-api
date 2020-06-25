240d12fd164c9192bb90046034452385adde4640
Junior Zytkowski <junior.zytkowski@gmail.com>
2020-06-24 22:58:35 -0300

    Enabled JPA Auditing

--- src/main/kotlin/com/bismuth/bismuth/BismuthApplication.kt
+++ src/main/kotlin/com/bismuth/bismuth/BismuthApplication.kt
@@ -2,8 +2,10 @@ package com.bismuth.bismuth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class BismuthApplication
 
 fun main(args: Array<String>) {
