2af90899a0ba88d1ad5458ecd4bbf3cf13499ee8
Junior Zytkowski <junior.zytkowski@gmail.com>
2020-06-25 21:08:42 -0300

    Added softdeleted_at, and softdeleted fields in the user migration

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -2,12 +2,14 @@
 -- DROP TABLE IF EXISTS public."user" CASCADE;
 CREATE TABLE public."user"
 (
-    user_id    uuid                     NOT NULL,
-    username   character varying(100)   NOT NULL,
-    email      character varying(255)   NOT NULL,
-    password   character varying(500)   NOT NULL,
-    created_at timestamp with time zone NOT NULL DEFAULT now(),
-    updated_at timestamp with time zone,
+    user_id        uuid                     NOT NULL,
+    username       character varying(100)   NOT NULL,
+    email          character varying(255)   NOT NULL,
+    password       character varying(500)   NOT NULL,
+    created_at     timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at     timestamp with time zone,
+    softdeleted_at timestamp with time zone,
+    softdeleted    bool                              DEFAULT false,
     CONSTRAINT pk_user PRIMARY KEY (user_id),
     CONSTRAINT unique_username UNIQUE (username),
     CONSTRAINT unique_email UNIQUE (email)
