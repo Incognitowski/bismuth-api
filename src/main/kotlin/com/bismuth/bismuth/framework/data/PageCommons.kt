82dc5ccbe046715e50711e86bd7190253bdeee7e
Junior Zytkowski <junior.zytkowski@gmail.com>
2020-08-26 22:30:38 -0300

    PageCommons Correction

--- src/main/kotlin/com/bismuth/bismuth/framework/data/PageCommons.kt
+++ src/main/kotlin/com/bismuth/bismuth/framework/data/PageCommons.kt
@@ -2,14 +2,19 @@ package com.bismuth.bismuth.framework.data
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
+
 
 class PageCommons {
     companion object {
         fun <T> getPaged(pageable: Pageable, listOfObjects: List<T>): Page<T> {
-            val start = pageable.offset.toInt();
-            val end = (if (start + pageable.pageSize > listOfObjects.size) listOfObjects.size else start + pageable.pageSize);
-            return PageImpl<T>(listOfObjects.subList(start, end), pageable, listOfObjects.size.toLong());
+            var end: Int = (pageable.pageNumber * pageable.pageSize) - 1;
+            val start: Int = (pageable.pageNumber - 1) * pageable.pageSize;
+            if(end > listOfObjects.size)
+                end = listOfObjects.size - 1;
+            val pageRequest: PageRequest = PageRequest.of(pageable.pageNumber - 1, pageable.pageSize, pageable.sort);
+            return PageImpl<T>(listOfObjects.subList(start, end + 1), pageRequest, listOfObjects.size.toLong());
         }
     }
 }
